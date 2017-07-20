f1241e8763f28df4f2bc47737d3e93ee0c753b2a
Darrin Bennett <boristotle@hotmail.com>
2017-07-20 09:41:09 -0500

    updates ;

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ let app = express()
 let path = require('path');
  
 app.use(express.static(path.join(__dirname, 'build')))
+app.use(express.static(path.join(__dirname, 'public')))
 
 app.get('/', function(req, res, next){
   res.sendFile(path.join(__dirname, 'build/index.html'));
